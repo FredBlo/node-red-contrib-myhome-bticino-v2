462b38946021dd26a045688c73cb0f38c0cf8bca
FredBlo <block_frederic@yahoo.fr>
2021-04-11 11:43:12 +0200

    temp node gets cur temp & zone temp objective

--- myhome-temperature.js
+++ myhome-temperature.js
@@ -8,6 +8,8 @@ module.exports = function (RED) {
     var node = this;
     let gateway = RED.nodes.getNode (config.gateway);
     let runningMonitor = new mhutils.eventsMonitor (gateway);
+    var lastTemp = '?';
+    var lastTempObj = '?';
 
     // Register node for updates
     node.on ('input', function (msg) {
@@ -31,23 +33,36 @@ module.exports = function (RED) {
       let payload = msg.payload;
 
       // Check whether received command is linked to current configured node shutter group/id
-      // From OpenWebNetDoc : *#4*where*12*T*3##
+      // From OpenWebNetDoc :
+      //  - current temperature (master probe) : *#4*where*0*T##
+      //  - current temperature goal set : *#4*where*14*T*3##
       //    The T field is composed from 4 digits c1c2c3c4, included between “0020” (2°temperature) and “0430” (43°temperature).
       //    c1 is always equal to 0, it indicates a positive temperature. The c2c3 couple indicates the temperature values between [02° - 43°].
-      let m = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*0\\*\\d(\\d\\d\\d)##');
+      let m = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*(0|14)\\*\\d(\\d\\d\\d)(\\*3|)##');
       if (m === null) {
         return;
       }
 
+      if (m[1] === '0') {
+        // Current temperature from master probe
+        lastTemp = parseInt (m[2]) / 10;
+      } else if (m[1] === '14') {
+        // Current temperature objective set
+        lastTempObj = parseInt (m[2]) / 10;
+      } else {
+        // Unmanaged information
+        return;
+      }
 
-      let curTemp = parseInt (m[1]) / 10;
-      node.status ({fill: 'grey', shape: 'ring', text: curTemp & '°C'});
+      node.status ({fill: 'grey', shape: 'ring', text: (lastTemp + '°C (Goal: ' + lastTempObj + '°C)' )});
 
       // Additional info : include initial SCS/BUS message which was read in payload
       payload.command_received = packet;
       // Build secondary payload
 //      let msg2 = mhutils.buildSecondaryOutput (payload.state, config, 'On', 'OPEN', 'CLOSE');
 
+      payload.state = lastTemp;
+      payload.state_tempobjective = lastTempObj;
       msg.topic = 'state/' + config.topic;
       node.send(msg);
     };
