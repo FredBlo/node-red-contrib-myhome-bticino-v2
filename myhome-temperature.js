4604adb61b1d27a278e4ecd772432635badfad64
FredBlo <block_frederic@yahoo.fr>
2021-04-11 20:19:34 +0200

    getting OL (offset)

--- myhome-temperature.js
+++ myhome-temperature.js
@@ -12,6 +12,7 @@ module.exports = function (RED) {
     // All current zone stored received values
     node.curTemp = '?';
     node.setTemp = '?';
+    node.localOffset_TechValue = '?';
     node.localOffset = '?';
     node.operationMode = '?';
 
@@ -36,44 +37,61 @@ module.exports = function (RED) {
       }
       let payload = msg.payload;
 
-      // Check whether received command is linked to current configured node shutter group/id
-      // From OpenWebNetDoc :
+      // Check whether received command is linked to current configured Zone
+      let packetMatch;
+      // Checks 1 : current temp and set objective (OpenWebNet doc) :
       //  - current temperature (master probe) : *#4*where*0*T##
       //  - current temperature goal set : *#4*where*14*T*3##
       //    The T field is composed from 4 digits c1c2c3c4, included between “0020” (2°temperature) and “0430” (43°temperature).
       //    c1 is always equal to 0, it indicates a positive temperature. The c2c3 couple indicates the temperature values between [02° - 43°].
-
-      //node.localOffset = '?'; // TODO
-          // *#4*where*13*OL##
-          // Local offset acquire frame:
-          // where = [1-99]
-          // OL = Local Offset (knob status):
-          // 00  knob on 0
-          // 01  knob on +1 (degree)
-          // 11  knob on -1 (degree)
-          // 02  knob on +2 (degree)
-          // 12  knob on -2 (degree)
-          // 03  knob on +3 (degree)
-          // 13  knob on -3 (degree)
-          // 4  knob on Local OFF
-          // 5  knob on Local protection
-      let m = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*(0|14)\\*\\d(\\d\\d\\d)(\\*3|)##');
-      if (m === null) {
-        return;
+      packetMatch = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*(0|14)\\*\\d(\\d\\d\\d)(\\*3|)##');
+      if (packetMatch !== null) {
+        if (packetMatch[1] === '0') {
+          // Current temperature from master probe
+          node.curTemp = parseInt (packetMatch[2]) / 10;
+        } else if (packetMatch[1] === '14') {
+          // Current temperature objective set
+          node.setTemp = parseInt (packetMatch[2]) / 10;
+        }
       }
-
-      if (m[1] === '0') {
-        // Current temperature from master probe
-        node.curTemp = parseInt (m[2]) / 10;
-      } else if (m[1] === '14') {
-        // Current temperature objective set
-        node.setTemp = parseInt (m[2]) / 10;
-      } else {
-        // Unmanaged information
-        return;
+      // Checks 2 : Local offset acquire frame (OpenWebNet doc) : *#4*where*13*OL## with OL = Local Offset (knob status):
+      // 00 = 0 / 01 = +1° / 02 = +2° / 03 = +3° / 11 = -1° / 12 = -2° / 13 = -3° / 4 = Local OFF / 5 = Local protection
+      if (packetMatch === null) {
+        packetMatch = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*(13)\\*(\\d\\d)##');
+        if (packetMatch !== null) {
+          node.localOffset_TechValue = packetMatch[2];
+          switch (node.localOffset_TechValue) {
+            case '00':
+              node.localOffset = 0;
+              break;
+            case '01':
+              node.localOffset = 1;
+              break;
+            case '02':
+              node.localOffset = 2;
+              break;
+            case '03':
+              node.localOffset = 3;
+              break;
+            case '11':
+              node.localOffset = -1;
+              break;
+            case '12':
+              node.localOffset = -2;
+              break;
+            case '13':
+              node.localOffset = -3;
+              break;
+            case '4':
+              node.localOffset = 'Local OFF';
+              break;
+            case '5':
+              node.localOffset = 'Local Protection';
+              break;
+          }
+        }
       }
-
-      node.status ({fill: 'grey', shape: 'ring', text: (node.curTemp + '°C (Goal: ' + node.setTemp + '°C)' )});
+      node.status ({fill: 'grey', shape: 'ring', text: (node.curTemp + '°C (Goal: ' + node.setTemp + '°C / Offset: ' + node.localOffset + ')' )});
 
       // Additional info : include initial SCS/BUS message which was read in payload
       payload.command_received = packet;
@@ -82,18 +100,7 @@ module.exports = function (RED) {
 
       payload.state = node.curTemp;
       payload.state_setTemperature = node.setTemp;
-
       payload.state_operationMode = node.operationMode; // TODO
-          //*4*what*where##
-          // Zone operation mode acquire frame:
-          // where = [1-99]
-          // what =
-          // 0  Conditioning
-          // 1  Heating
-          // 102  Antifreeze
-          // 202  Thermal Protection
-          // 303  Generic OFF
-
       payload.state_localOffset = node.localOffset;
       msg.topic = 'state/' + config.topic;
       node.send(msg);
