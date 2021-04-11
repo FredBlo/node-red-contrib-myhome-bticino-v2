128a367a36ea80914806aee100619d68585f8762
FredBlo <block_frederic@yahoo.fr>
2021-04-11 16:10:43 +0200

    local offset & running mode OWN search doc

--- myhome-temperature.js
+++ myhome-temperature.js
@@ -8,8 +8,12 @@ module.exports = function (RED) {
     var node = this;
     let gateway = RED.nodes.getNode (config.gateway);
     let runningMonitor = new mhutils.eventsMonitor (gateway);
-    var lastTemp = '?';
-    var lastTempObj = '?';
+
+    // All current zone stored received values
+    node.curTemp = '?';
+    node.setTemp = '?';
+    node.localOffset = '?';
+    node.operationMode = '?';
 
     // Register node for updates
     node.on ('input', function (msg) {
@@ -38,6 +42,21 @@ module.exports = function (RED) {
       //  - current temperature goal set : *#4*where*14*T*3##
       //    The T field is composed from 4 digits c1c2c3c4, included between “0020” (2°temperature) and “0430” (43°temperature).
       //    c1 is always equal to 0, it indicates a positive temperature. The c2c3 couple indicates the temperature values between [02° - 43°].
+
+      //node.localOffset = '?'; // TODO
+          // *#4*where*13*OL##
+          // Local offset acquire frame:
+          // where = [1-99]
+          // OL = Local Offset (knob status):
+          // 00  knob on 0
+          // 01  knob on +1 (degree)
+          // 11  knob on -1 (degree)
+          // 02  knob on +2 (degree)
+          // 12  knob on -2 (degree)
+          // 03  knob on +3 (degree)
+          // 13  knob on -3 (degree)
+          // 4  knob on Local OFF
+          // 5  knob on Local protection
       let m = packet.match ('^\\*\\#4\\*' + config.zoneid + '\\*(0|14)\\*\\d(\\d\\d\\d)(\\*3|)##');
       if (m === null) {
         return;
@@ -45,24 +64,37 @@ module.exports = function (RED) {
 
       if (m[1] === '0') {
         // Current temperature from master probe
-        lastTemp = parseInt (m[2]) / 10;
+        node.curTemp = parseInt (m[2]) / 10;
       } else if (m[1] === '14') {
         // Current temperature objective set
-        lastTempObj = parseInt (m[2]) / 10;
+        node.setTemp = parseInt (m[2]) / 10;
       } else {
         // Unmanaged information
         return;
       }
 
-      node.status ({fill: 'grey', shape: 'ring', text: (lastTemp + '°C (Goal: ' + lastTempObj + '°C)' )});
+      node.status ({fill: 'grey', shape: 'ring', text: (node.curTemp + '°C (Goal: ' + node.setTemp + '°C)' )});
 
       // Additional info : include initial SCS/BUS message which was read in payload
       payload.command_received = packet;
       // Build secondary payload
 //      let msg2 = mhutils.buildSecondaryOutput (payload.state, config, 'On', 'OPEN', 'CLOSE');
 
-      payload.state = lastTemp;
-      payload.state_tempobjective = lastTempObj;
+      payload.state = node.curTemp;
+      payload.state_setTemperature = node.setTemp;
+
+      payload.state_operationMode = node.operationMode; // TODO
+          //*4*what*where##
+          // Zone operation mode acquire frame:
+          // where = [1-99]
+          // what =
+          // 0  Conditioning
+          // 1  Heating
+          // 102  Antifreeze
+          // 202  Thermal Protection
+          // 303  Generic OFF
+
+      payload.state_localOffset = node.localOffset;
       msg.topic = 'state/' + config.topic;
       node.send(msg);
     };
